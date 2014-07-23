8788cb4fc3392e0ab47ff26bc30324fb63b69b5e
yangbo1979 <gyangbo@gmail.com>
2014-07-23 13:33:00 +0800

    Update append_send.ino

--- examples/append_send/append_send.ino
+++ examples/append_send/append_send.ino
@@ -13,6 +13,13 @@
 //delay between updates
  #define POST_INTERVAL (30*1000)
  
+byte mac[] = {0x74,0x69,0x69,0x2D,0x30,0x31};
+
+//IPAddress ip(192,168,1, 15);
+//IPAddress mydns(8,8,8,8);
+//IPAddress gw(192,168,1,1);
+//IPAddress subnet(255,255,255,0);
+ 
 LeWeiClient *lwc;
  
  
@@ -21,6 +28,7 @@ void setup() {
    Serial.begin(9600);
    // hope no exception here
    lwc = new LeWeiClient(LW_USERKEY, LW_GATEWAY);
+   //lwc = new LeWeiClient(LW_USERKEY, LW_GATEWAY,mac,ip,mydns,gw,subnet);
  }
  
 void loop() {
